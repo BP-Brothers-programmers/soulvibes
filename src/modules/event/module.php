967ed29938aba9494c3f9d319fb51a0ba35f9b15
Konstantin <skripkin_kostyunya@mail.ru>
2018-10-08 16:39:54 +0600

    Events

---
+++ src/modules/event/module.php
@@ -0,0 +1,6 @@
+<section class="event">
+    <p class="event__time"><?=$event['time']?></p>
+    <h2 class="event__title"><?=$event['title']?></h2>
+    <p class="event__name"><?=$event['name']?></p>
+    <p class="event__address"><?=$event['address']?></p>
+</section>
---
+++ src/parts/upcoming-events/module.php
@@ -0,0 +1,34 @@
+<div class="upcoming-events">
+    <div class="upcoming-events__event-info container">
+        <div class="upcoming-events__scroll">
+            <?php $event = [
+                    'time' => 'Wednesday / 22 Dec 2017 / 12:00 PM',
+                    'title' => 'Linkin Park',
+                    'name' => 'Ziggo Dome',
+                    'address' => 'Lorem ipsum dolor sit amet.'
+                ];
+                include "../src/modules/event/module.php"; ?>
+            <?php $event = [
+                    'time' => 'Wednesday / 22 Dec 2017 / 12:00 PM',
+                    'title' => 'Linkin Park',
+                    'name' => 'Ziggo Dome',
+                    'address' => 'Lorem ipsum dolor sit amet.'
+                ];
+                include "../src/modules/event/module.php"; ?>
+            <?php $event = [
+                    'time' => 'Wednesday / 22 Dec 2017 / 12:00 PM',
+                    'title' => 'Linkin Park',
+                    'name' => 'Ziggo Dome',
+                    'address' => 'Lorem ipsum dolor sit amet.'
+                ];
+                include "../src/modules/event/module.php"; ?>
+            <?php $event = [
+                    'time' => 'Wednesday / 22 Dec 2017 / 12:00 PM',
+                    'title' => 'Linkin Park',
+                    'name' => 'Ziggo Dome',
+                    'address' => 'Lorem ipsum dolor sit amet.'
+                ];
+                include "../src/modules/event/module.php"; ?>
+        </div>
+    </div>
+</div>
